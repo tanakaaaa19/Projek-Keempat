38653be6a24c5f25065b57d8942427026274f605
tanakaaaa19 <tanakakunohtakun@gmail.com>
2026-02-06 06:17:18 +0000

    Menambahkan struktur fungsi tambah_catatan() untuk menambahkan catatan

---
+++ main.py
@@ -0,0 +1,46 @@
+catatan = []
+
+def tambah_catatan():
+    mapel = input("Masukkan nama mapel: ")
+    topik = input("Masukkan topik belajar: ")
+    durasi = input("Masukkan durasi belajar (menit): ")
+    
+    # Menyimpan data dalam bentuk dictionary
+    catatan_baru = {
+        "mapel": mapel,
+        "topik": topik,
+        "durasi": int(durasi)
+    }
+    
+    # Menambahkan ke list catatan
+    catatan.append(catatan_baru)
+    print("✓ Catatan berhasil ditambahkan!\n")
+
+def lihat_catatan():
+    pass
+
+def total_waktu():
+    pass
+
+def menu():
+    print("\n=== Study Log App ===")
+    print("1. Tambah catatan belajar")
+    print("2. Lihat catatan belajar")
+    print("3. Total waktu belajar")
+    print("4. Keluar")
+
+while True:
+    menu()
+    pilihan = input("Pilih menu: ")
+
+    if pilihan == "1":
+        tambah_catatan()
+    elif pilihan == "2":
+        lihat_catatan()
+    elif pilihan == "3":
+        total_waktu()
+    elif pilihan == "4":
+        print("Terima kasih, terus semangat belajar!")
+        break
+    else:
+        print("Pilihan tidak valid")
